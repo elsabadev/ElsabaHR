dcf2081860ce04132a09b7fc109315433aa80086
Eslam <eslam-abdallah@elsabaautoservice.com>
2019-01-08 16:58:31 +0200

    Create Log In Activity

---
+++ app/src/main/java/com/elsabaautoservice/www/elsabahr/Log_IN.java
@@ -0,0 +1,13 @@
+package com.elsabaautoservice.www.elsabahr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Log_IN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log__in);
+    }
+}
--- app/src/main/java/com/elsabaautoservice/www/elsabahr/MainActivity.java
+++ app/src/main/java/com/elsabaautoservice/www/elsabahr/MainActivity.java
@@ -1,16 +1,43 @@
 package com.elsabaautoservice.www.elsabahr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Timer timer ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Test
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+
+        );
+
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Log_IN.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
 
